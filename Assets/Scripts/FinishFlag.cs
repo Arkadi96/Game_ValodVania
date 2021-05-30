e5fc53d77df586cac2f5b523e3439f7b3f37da4d
Arkadi96 <arkadi.hakobyan.96@mail.ru>
2021-05-30 15:09:15 +0400

    Update 4
    Added starting and finishing flags
    Added a scene manager

---
+++ Assets/Scripts/FinishFlag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishFlag : MonoBehaviour
+{
+    //chached references
+    private SceneManagerController sceneManagerController;
+
+    //configuration parameters
+    [Range(0f, 1f)] [SerializeField] private float frozenTime=0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneManagerController = FindObjectOfType<SceneManagerController>();    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger entered");
+        StartCoroutine(LoadNextSceneAnimation());        
+    }
+
+    IEnumerator LoadNextSceneAnimation()
+    {
+        Time.timeScale = frozenTime;
+        yield return new WaitForSeconds(1.0f);
+        Time.timeScale = 1;
+        sceneManagerController.LoadNextScene();
+    }
+}
---
+++ Assets/Scripts/SceneManagerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerController : MonoBehaviour
+{
+    //chached string parameters
+    private string MAIN_MENU_SCENE = "Main_Menu";
+    private string GAME_OVER_SCENE = "Game_Over";
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(MAIN_MENU_SCENE);
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(GAME_OVER_SCENE);
+    }
+
+    public void LoadCertainScene(string CERTAIN_SCENE_NAME)
+    {
+        bool sceneExists = false;
+        int numberOfScenes = SceneManager.sceneCount;
+
+        for (int i =0; i<=numberOfScenes;i++)
+        {
+            if (SceneManager.GetSceneByBuildIndex(i).name.Equals(CERTAIN_SCENE_NAME))
+            {
+                sceneExists = true;
+            }
+        }         
+
+        if (sceneExists)
+        {
+            SceneManager.LoadScene(CERTAIN_SCENE_NAME);
+        }
+        else
+        {
+            Debug.LogError("Cannot find given scene "+CERTAIN_SCENE_NAME);
+        }
+    }
+}
